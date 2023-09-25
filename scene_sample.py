1349a79545424613e26be150582102f5b99f4a28
momiji_5717 <momiji5717@gmail.com>
2023-09-26 00:28:06 +0800

    camera added

--- scene_sample.py
+++ scene_sample.py
@@ -7,11 +7,18 @@ from gym import error,spaces,utils
 from gym.utils import seeding
 import numpy as np
 
-from time import sleep
+import pkgutil
+egl = pkgutil.get_loader('eglRenderer')
+from time import sleep, time
+
+import matplotlib.pyplot as plt
 
 class PushEnv(gym.Env):
     def __init__(self):
+        # p.connect(p.GUI)/
         p.connect(p.GUI)
+        # p.connect(p.DIRECT)
+        
         
         self.action_space = spaces.Box(low=np.ones(3)*-1, high=np.ones(3)) # eef vel: [vx, vy, vz]
         self.observation_space = gym.spaces.Dict({
@@ -19,9 +26,27 @@ class PushEnv(gym.Env):
             'eef_pos': gym.spaces.Box(low=np.ones(3)*-1, high=np.ones(3), dtype=np.float32)
         })
         
-        self.dv = 10 / 240
+        
+        dt = 1./20.
+        p.setTimeStep(dt)
+        self.dv = 5 * dt
+        self.visPitch = -50
+        self.visYaw = 50
+        self.visDis = 1.5
+        self.visTargetPos = [0.5, 0, 0]
+    
+        p.resetDebugVisualizerCamera(cameraDistance=self.visDis, 
+                                     cameraYaw=self.visYaw, 
+                                     cameraPitch=self.visPitch,
+                                     cameraTargetPosition=self.visTargetPos)
+        
+        p.setAdditionalSearchPath(pd.getDataPath())
         
     def reset(self):
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,0)
+        # p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+        # p.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW, 1)
+        
         p.resetSimulation()
 
         self.tableUid = p.loadURDF("models/table/table.urdf",basePosition=[0.5,0,-0.63])
@@ -35,15 +60,18 @@ class PushEnv(gym.Env):
 
         # load the panda robot and set its joint angles 
         self.pandaUid = p.loadURDF("models/franka_panda/panda.urdf",useFixedBase=True)
+        # planeId = p.loadURDF("plane.urdf",  basePosition=[0, 0, -0.63])
         
         # initialize joint position
         rest_angles = [0, -pi/4, 0, -3*pi/4, 0, pi/2, pi/4]
         for i, angle in enumerate(rest_angles):
             p.resetJointState(self.pandaUid, i, angle)
             
-
+        p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,1)
         
     def step(self, action):
+        p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
+        
         # --------- robot control ----------
         dx, dy, dz = action
         dx *= self.dv
@@ -78,16 +106,43 @@ class PushEnv(gym.Env):
         ObjPosition = ObjState[0]
         ObjOrientation = ObjState[1] # the orientation is in quaternion form
         
+        img = self.render()
+        # plt.imshow(img)
+        # plt.show()
+        
         # PLEASE modify the reward function in this part 
         
-        if False: # add terminal condition
+        if False: # set the terminal condition
             done = True
         
         
         return obs, reward, done, info
         
     def render(self):
-        pass
+        H, W = 240, 240
+        view_matrix=p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=self.visTargetPos,
+                                                        distance=self.visDis,
+                                                        yaw=self.visYaw,
+                                                        pitch=self.visPitch,
+                                                        roll=0,
+                                                        upAxisIndex=2)
+        
+        proj_matrix=p.computeProjectionMatrixFOV(fov=60,aspect=float(W)/H,
+                                                 nearVal=0.1,
+                                                 farVal=100.0)
+        
+        (_,_,px,_,_)=p.getCameraImage(width=W,
+                                      height=H,
+                                      viewMatrix=view_matrix,
+                                      projectionMatrix=proj_matrix,
+                                      renderer=p.ER_BULLET_HARDWARE_OPENGL, 
+                                      flags=p.ER_NO_SEGMENTATION_MASK)
+        
+        rgb_array=np.array(px,dtype=np.uint8)
+        # rgb_array=np.reshape(rgb_array,(H,W,4))
+        rgb_array=rgb_array[:, :, :3]
+
+        return rgb_array
         
     def close(self):
         p.disconnect()
@@ -98,8 +153,10 @@ if __name__ == "__main__":
     env.reset()
     
     i = 0
-    while i < 240:
+
+    t1 = time()
+    while i < 20*10:
         env.step([0, 0, -1])
-        sleep(1/240)
+        i += 1
         
-        # i += 1
+    print(f"\n\nsimulated 10 seconds in {time() - t1} seconds\n\n")
